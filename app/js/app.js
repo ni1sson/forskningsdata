4be186e348e00643d79d0123570b0852af5c977b
Marcus Kempe <makem86@gmail.com>
2014-05-14 10:05:34 +0200

    Added route for projects

--- app/js/app.js
+++ app/js/app.js
@@ -13,16 +13,29 @@ var phonecatApp = angular.module('phonecatApp', [
 
 phonecatApp.config(['$routeProvider',
   function($routeProvider) {
-    $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
+    // $routeProvider.
+    //   when('/phones', {
+    //     templateUrl: 'partials/phone-list.html',
+    //     controller: 'PhoneListCtrl'
+    //   }).
+    //   when('/phones/:phoneId', {
+    //     templateUrl: 'partials/phone-detail.html',
+    //     controller: 'PhoneDetailCtrl'
+    //   }).
+    //   otherwise({
+    //     redirectTo: '/phones'
+    //   });
+      
+    $routeProider.
+      when('/projects', {
+        templateUrl: 'partials/project-list.html',
+        controller: 'ProjectListCtrl'
       }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
+      when('/projects/:projectId',{
+        templateUrl: 'partials/project-detail.html',
+        controller: 'ProjectDetailCtrl'
       }).
       otherwise({
-        redirectTo: '/phones'
+        redirectTo: '/projects'
       });
   }]);
