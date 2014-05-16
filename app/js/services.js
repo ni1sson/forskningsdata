f0b9f8a737f4a3bc74439fcb19a33e45014b3c3a
ni1sson <johan@grandala.se>
2014-05-16 15:48:04 +0200

    update

--- app/js/services.js
+++ app/js/services.js
@@ -29,6 +29,10 @@ forskningsdataServices.factory('Weather', ['$resource',
 	}
 ]);
 
+forskningsdataServices.factory('SessionService', function($resource) {
+  return $resource('/api/sessions');
+});
+
 forskningsdataServices.factory("page", function($rootScope){
     var page={};
     var user={};
